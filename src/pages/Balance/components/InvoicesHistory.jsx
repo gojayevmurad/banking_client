bb8d83a05bc2629b8df4371c995422571c325c27
Murad Gojayev <muradqocayevi20@gmail.com>
2023-06-18 22:55:27 +0400

    balance page static comp.

--- src/pages/Balance/components/InvoicesHistory.jsx
+++ src/pages/Balance/components/InvoicesHistory.jsx
@@ -1,9 +1,96 @@
-import React from 'react'
+import React, { useState } from "react";
+
+import logo from "../../../assets/user_photo.jpg";
+
+const returnTrueIcon = (option) => {
+  switch (option) {
+    case -1:
+      return (
+        <div data-color="red" className="icon">
+          <div data-bg="red" className="bg"></div>
+          <p>!</p>
+        </div>
+      );
+    case 0:
+      return (
+        <div data-color="grey" className="icon">
+          <div data-bg="grey" className="bg"></div>
+          <p>?</p>
+        </div>
+      );
+    case 1:
+      return (
+        <div data-color="green" className="icon">
+          <div data-bg="green" className="bg"></div>
+          <p>+</p>
+        </div>
+      );
+    default:
+      break;
+  }
+};
 
 const InvoicesHistory = () => {
-  return (
-    <div>InvoicesHistory</div>
-  )
-}
+  const [invoices, setInvoices] = useState({
+    data: [
+      {
+        title: "Karen Hope",
+        time: "1h ago",
+        profile_photo: logo,
+        status: -1,
+      },
+      {
+        title: "Tony Soap",
+        time: "1h ago",
+        profile_photo: logo,
+        status: -1,
+      },
+      {
+        title: "Jordan Nico",
+        time: "1h ago",
+        profile_photo: logo,
+        status: 0,
+      },
+      {
+        title: "Samantha W.",
+        time: "1h ago",
+        profile_photo: logo,
+        status: 0,
+      },
+      {
+        title: "Jhonny A.",
+        time: "1h ago",
+        profile_photo: logo,
+        status: 1,
+      },
+    ],
+    total: 20,
+  });
 
-export default InvoicesHistory
+  return (
+    <div className="invoices_history">
+      <h4>Invoices Sent</h4>
+      <div className="invoices_history--content">
+        {invoices.data.map((item, index) => {
+          return (
+            <div key={index}>
+              <div className="desc">
+                <div className="img">
+                  <img src={item.profile_photo} alt="pp" />
+                </div>
+                <div className="data">
+                  <p>{item.title}</p>
+                  <span>{item.time}</span>
+                </div>
+              </div>
+              <div className="status">{returnTrueIcon(item.status)}</div>
+            </div>
+          );
+        })}
+      </div>
+      {invoices.total > 5 && <button className="view_more">View more</button>}
+    </div>
+  );
+};
+
+export default InvoicesHistory;
--- src/pages/Balance/components/TransferHistory.jsx
+++ src/pages/Balance/components/TransferHistory.jsx
@@ -1,12 +1,93 @@
-import React from 'react'
+import React, { useState } from "react";
+import { formatMoney } from "../../../utils";
+import logo from "../../../assets/user_photo.jpg";
 
 const TransferHistory = () => {
-  return (
-    <div className='transfer_history'>
-      <h5>Transfer History</h5>
-      
-    </div>
-  )
-}
+  const [transferHistory, setTransferHistory] = useState([
+    {
+      title: "Samantha Wall",
+      profile_photo: logo,
+      income: false,
+      amount: 123523,
+      status: -1,
+      operation_id: 1234567,
+      date: "09 Dec, 2020",
+      time: "04:23:32 AM",
+    },
+    {
+      title: "Samantha Wall",
+      profile_photo: logo,
+      income: false,
+      amount: 123594,
+      status: 1,
+      operation_id: 1234567,
+      date: "25 Jan, 2021",
+      time: "04:34:45 AM",
+    },
+    {
+      title: "Kren Hopo",
+      profile_photo: logo,
+      income: true,
+      amount: 8345924,
+      status: 0,
+      operation_id: 1234567,
+      date: "15 Jul, 2022",
+      time: "10:44:45 PM",
+    },
+  ]);
 
-export default TransferHistory
+  return (
+    <div className="transfer_history">
+      <h4>Transfer History</h4>
+      <div className="transfer_history--content">
+        {transferHistory.map((item, index) => {
+          return (
+            <div key={index}>
+              <div className="item_main">
+                <div className="icon">
+                  <img src={item.profile_photo} alt="" />
+                </div>
+                <div className="person">
+                  <p>{item.title}</p>
+                </div>
+              </div>
+              <p className="operation_id">#{item.operation_id}</p>
+              <p className="amount">$ {formatMoney(item.amount)}</p>
+              <div className="date">
+                <p>{item.date}</p>
+                <span>{item.time}</span>
+              </div>
+              <p
+                data-color={
+                  item.status == -1
+                    ? "red"
+                    : item.status == 0
+                    ? "grey"
+                    : "green"
+                }
+                className="status"
+              >
+                {item.status == -1
+                  ? "Canceled"
+                  : item.status == 0
+                  ? "Pending"
+                  : "Completed"}
+              </p>
+              <button className="more_btn">•••</button>
+            </div>
+          );
+        })}
+      </div>
+      <div className="controls">
+        <p>Showing 1-5 from 100 data</p>
+        <div className="pagination">
+          <button className="active">1</button>
+          <button>2</button>
+          <button>3</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TransferHistory;
